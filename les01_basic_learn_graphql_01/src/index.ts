8fc40c8a899d031b9704651c312e5330d34d55cb
Phuc Do Ngoc <ngocphucdo2601@gmail.com>
2023-09-03 15:24:45 +0700

    [learn-graphql-fun-01] 04 Load nested data on graphql

--- les01_basic_learn_graphql_01/src/index.ts
+++ les01_basic_learn_graphql_01/src/index.ts
@@ -11,21 +11,39 @@ const resolvers = {
         games() {
             return db.games;
         },
-        authors(){
+        authors() {
             return db.authors;
         },
         reviews() {
             return db.reviews;
         },
         review(_, args) {
-            
+
             return db.reviews.find((review) => review.id === args.id);
         },
-        game(_,  args) {
+        game(_, args) {
             return db.games.find((game) => game.id === args.id);
         },
         author(_, args) {
             return db.authors.find((author) => author.id === args.id);
+        }
+    },
+    Game: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.game_id === parent.id);
+        }
+    },
+    Author: {
+        reviews(parent) {
+            return db.reviews.filter((review) => review.author_id === parent.id);
+        }
+    },
+    Review: {
+        author(parent) {
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent) {
+            return db.games.find((g) => g.id === parent.game_id)
         }
     }
 }
